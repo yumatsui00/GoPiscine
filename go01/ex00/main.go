7c867aa50735ca07993e7c671fde74a21278082d
Yuma Matsui <yumatsui@c1r2s3.42tokyo.jp>
2024-06-12 18:24:40 +0900

    gogo

---
+++ go01/ex00/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"piscine"
+)
+
+func main()
+{
+	piscine.PrintAlphabet();
+}
+
+// go mod init & go mod tidy to clear dependency
+// go fmt to see if the format is correct
+// go vet to find unclear wrong format
+// go run file to compile and run and delete the execxutable file
+// go build to make an executable file. Then ./ex00
